df05c37d34da227fe7f4b1579f22440d725c0208
Sam_Rytech <107815081+Sam-Rytech@users.noreply.github.com>
2026-04-17 01:10:45 +0100

    Add project_verification to other icons

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -17,6 +17,9 @@ export const metadata: Metadata = {
    icons: {
     icon: "/Automata_Icon.png",
   },
+  other: {
+    "talentapp:project_verification":"5f5af8c02bb7e41064702e5c4b5b747665490caf73efaffd2836c48d55405ab78eb321d9de07ad201b185c833e1d539e72de1208fb36a6670100d817db59be78"
+      }
 }
 
 
